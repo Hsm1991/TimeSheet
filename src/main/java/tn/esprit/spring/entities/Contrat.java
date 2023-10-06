d84b4bd387ab7081a26c1603f0ab461cc0453a81
houcem1307 <houssem.mhedhbi2017@gmail.com>
2023-10-06 10:18:45 +0100

    contrat changement

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -3,18 +3,20 @@ package tn.esprit.spring.entities;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class Contrat implements Serializable {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table( name = "Contrat")public class Contrat implements Serializable {
 	
 
 	/**
@@ -37,55 +39,8 @@ public class Contrat implements Serializable {
 
 	private float salaire;
 
-	public Contrat() {
-		super();
-	}
+
 	
-	public Contrat(Date dateDebut, String typeContrat, float salaire) {
-		this.dateDebut = dateDebut;
-		this.typeContrat = typeContrat;
-		this.salaire = salaire;
-	}
 
-
-	public Date getDateDebut() {
-		return dateDebut;
-	}
-
-	public void setDateDebut(Date dateDebut) {
-		this.dateDebut = dateDebut;
-	}
- 
-	public Long getReference() {
-		return reference;
-	}
-
-	public void setReference(Long reference) {
-		this.reference = reference;
-	}
-
-	public String getTypeContrat() {
-		return typeContrat;
-	}
-
-	public void setTypeContrat(String typeContrat) {
-		this.typeContrat = typeContrat;
-	}
-
-	public float getSalaire() {
-		return salaire;
-	}
-
-	public void setSalaire(float salaire) {
-		this.salaire = salaire;
-	}
-
-	public Employe getEmploye() {
-		return employe;
-	}
-
-	public void setEmploye(Employe employe) {
-		this.employe = employe;
-	}
  
 }
